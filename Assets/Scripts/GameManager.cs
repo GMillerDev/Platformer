1750926ffecc439f5ee17c95b5ea85fd4135c197
Xavier Dahlstet <xavierdahlstet1@gmail.com>
2026-09-15 19:40:15 -0700

    added out bounds respond

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,24 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject winScreen;
 
-    public bool won{ get; private set; }
+    [Header("Respawn")]
+    [SerializeField] private Rigidbody2D player;
+    [Tooltip("Where the player respawns. If left empty, the player's starting position is used.")]
+    [SerializeField] private Transform spawnPoint;
+
+    public bool won { get; private set; }
     public bool fellOutOfBounds { get; private set; }
+    public int deaths { get; private set; }
+
+    private Vector3 startPosition;
+
+    private void Start()
+    {
+        if (player != null)
+        {
+            startPosition = player.transform.position;
+        }
+    }
 
     public void Win()
     {
@@ -15,6 +31,23 @@ public class GameManager : MonoBehaviour
 
     public void FellOutOfBounds()
     {
+        if (won) return;
+
         fellOutOfBounds = true;
+        deaths++;
+        Respawn();
+    }
+
+    private void Respawn()
+    {
+        if (player == null) return;
+
+        Vector3 target = spawnPoint != null ? spawnPoint.position : startPosition;
+
+        player.linearVelocity = Vector2.zero;
+        player.position = target;
+        player.transform.position = target;
+
+        fellOutOfBounds = false;
     }
 }
--- Assets/Scripts/OutOfBoundsTrigger.cs
+++ Assets/Scripts/OutOfBoundsTrigger.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class OutOfBoundsTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameManager gameManager;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            gameManager.FellOutOfBounds();
+        }
     }
 }
